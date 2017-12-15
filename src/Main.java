6e94216e2a50f7d5fdba3c05bc80620d1370fabe
John Leon <jleonfollett@gmail.com>
2017-12-15 03:38:18 -0600

    Cleaned up

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) {
-		new Deck();
+		Deck one = new Deck();
+		new Simulation(one.getPlayerOne(), one.getPlayerTwo());
 	}
 
 }
